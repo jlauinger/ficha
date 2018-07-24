0a81f8ee91b39af4de4fb6c562b8fea527723dac
Johannes Lauinger <johannes@lauinger-it.de>
2018-07-25 00:20:29 +0200

    add collection title and card subcomponent to TrainerComponent

--- src/app/trainer/components/trainer/trainer.component.spec.ts
+++ src/app/trainer/components/trainer/trainer.component.spec.ts
@@ -1,5 +1,8 @@
 import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import {TrainerComponent} from './trainer.component';
+import {Component, Input} from '@angular/core';
+import {Card} from '../../../base/models/card/card.model';
+import {Collection} from '../../../base/models/collection/collection.model';
 
 
 describe('TrainerComponent', () => {
@@ -10,7 +13,8 @@ describe('TrainerComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
-                TrainerComponent
+                TrainerComponent,
+                CardStubComponent
             ],
         }).compileComponents();
     }));
@@ -23,4 +27,18 @@ describe('TrainerComponent', () => {
     it('should create', async(() => {
         expect(component).toBeTruthy();
     }));
+
+    it('should display title of current collection in a bold tag', () => {
+        component.collection = new Collection('SPANISH');
+
+        fixture.detectChanges();
+
+        expect(fixture.nativeElement.querySelector('b').innerText).toEqual('SPANISH');
+    });
 });
+
+
+@Component({selector: 'app-card', template: ''})
+class CardStubComponent {
+    @Input() card: Card;
+}
--- src/app/trainer/components/trainer/trainer.component.ts
+++ src/app/trainer/components/trainer/trainer.component.ts
@@ -1,10 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Card} from '../../../base/models/card/card.model';
+import {Collection} from '../../../base/models/collection/collection.model';
 
 @Component({
     selector: 'app-trainer',
     templateUrl: './trainer.component.html',
     styleUrls: ['./trainer.component.css']
 })
-export class TrainerComponent {
+export class TrainerComponent implements OnInit {
 
+    collection: Collection;
+    card: Card;
+
+    ngOnInit() {
+        this.collection = new Collection('SPANISH');
+        this.collection.add(new Card('ser', 'to be (trait)'));
+        this.collection.add(new Card('ser', 'to be (state, position)'));
+
+        this.card = this.collection.nextCard();
+    }
 }
